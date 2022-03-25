956ff52eb22a72121e72b8a80ef7783cb885bef3
Azunna Ikonne <ikonnea@gmail.com>
2022-03-25 09:56:56 +0100

    update requirements install
    Signed-off-by: Azunna Ikonne <ikonnea@gmail.com>

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -14,9 +14,14 @@ sudo apt install -y libglib2.0-dev libgpgme-dev libgnutls28-dev uuid-dev libssh-
     libnet1-dev libpq-dev postgresql-server-dev-11 postgresql-client-11 postgresql-11 redis-server libical-dev xsltproc rsync \
     nodejs yarnpkg libmicrohttpd-dev gcc-mingw-w64 libpopt-dev libunistring-dev heimdal-dev perl-base bison libgcrypt20-dev \
     libgpgme-dev libksba-dev nmap libldap2-dev libradcli-dev texlive-latex-extra texlive-fonts-recommended xmlstarlet zip rpm \
-    fakeroot dpkg nsis gnupg gpgsm sshpass openssh-client socat snmp smbclient python3-lxml gnutls-bin xml-twig-tools python3-impacket \
-    libsnmp-dev python3-setuptools python3-packaging python3-wrapt python3-cffi python3-psutil python3-defusedxml python3-paramiko python3-redis
+    fakeroot dpkg nsis gnupg gpgsm sshpass openssh-client socat snmp smbclient python3-lxml gnutls-bin xml-twig-tools libsnmp-dev \
+    python3-setuptools python3-packaging python3-wrapt python3-cffi python3-psutil python3-defusedxml python3-paramiko python3-redis
 
+#support for Debian and ubuntu versions
+sudo apt install -y python3-impacket
+if [ $? -ne 0 ]; then
+    sudo apt install -y python-impacket
+fi
 
 # Import GVM signing key
 curl -O https://www.greenbone.net/GBCommunitySigningKey.asc && \
